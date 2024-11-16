54946af7dfaa95932be591465854887faa940094
Martin Askestad <MartinAskestad@users.noreply.github.com>
2024-11-16 08:42:41 +0100

    Update

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
-#! /bin/sh
+#!/bin/sh
 
 export XDG_CONFIG_HOME="$HOME/.config"
-mkdir -p $XDG_CONFIG_HOME
+mkdir -p "$XDG_CONFIG_HOME"
 
-ln -s "$PWD/vim" "$XDG_CONFIG_HOME"/vim
+if [ ! -e "$XDG_CONFIG_HOME/vim" ]; then
+  ln -s "$PWD/vim" "$XDG_CONFIG_HOME/vim"
+  echo "Symbolic link created: $XDG_CONFIG_HOME/vim -> $PWD/vim"
+else
+  echo "Symbolic link already exists: $XDG_CONFIG_HOME/vim"
+fi
